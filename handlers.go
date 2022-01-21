addd61216413f4d756c4ada194cd70cb4f995741
Jeremy Chase <jeremy.chase@gmail.com>
2022-01-20 19:21:43 -0500

    workaround v%d aprs.fi issue

--- handlers.go
+++ handlers.go
@@ -12,14 +12,22 @@ import (
 )
 
 func v1(w http.ResponseWriter, req *http.Request) {
-	aprs.SwName = "wxigate-v"
-	aprs.SwVers = Version
-
 	wx := aprs.Wx{
 		Lat:  latitude,
 		Lon:  longitude,
 		Type: comment,
 	}
+
+	// SwName are SwVers are are concatenated in the 'comment' field and then
+	// immediately followed by the Wx.Type. This is performed in the upstream
+	// aprs library and looks like:
+	//
+	//   fmt.Sprintf("%s%s%s", aprs.SwName, aprs.SwVers, wx.Type)
+	//
+	// On aprs.fi a lowercase 'v%d' is get dropped, so that is why this
+	// is 'V'.
+	aprs.SwName = "wxigate-V"
+	aprs.SwVers = Version
 
 	wx.Timestamp = time.Now() // BUG(low) use "dateutc" in query
 
